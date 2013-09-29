f155f7c62fd384e2d7a2d22c1bffe639981bd39c
Arthur Flam <arthur.flam@gmail.com>
2013-09-29 14:41:45 +0200

    changed file names, started to make it compile

---
+++ fuzzySetsAPI.java
@@ -0,0 +1,71 @@
+class fuzzySetsAPI{
+
+    public static void main(String[] args){
+	System.out.println("Hello world.");
+	//data data = new data();
+    }
+    
+    /**
+    public static class ensemble{
+	public String name;
+	public point[] points;
+	
+	//Constructeurs
+	// TODO 
+
+	//Informations
+	public int length(){
+	    return points.length();
+	};
+	public String toString(){
+		String out = "";
+		//for(int i = 0; i < points.length(); i++){
+		//	out = out + point[i].toString;
+		//}
+		return out;
+	}
+	public double valueAt(double x){
+		//Trouver x1 < x < x2
+		// par dichotomie ? A voir en cas d'appels multiples
+		//return (y2-y1)/(x2-x1) * (x1-x);
+	}
+
+	//Opérations ensemblistes
+	public static ensemble union(ensemble A, ensemble B, String method){}
+	public static ensemble intersection(ensemble A, ensemble B, String method){}
+	public static ensemble complementaire(){}
+	//public static ensemble apply(ensemble A, math.function f){}{
+	    // Premier algo possible
+	    // calcul des valeurs des points (x,y) de A -> double[][] (f(x), y)
+	    // Si on calcul un f(x) déjà obtenu, on prend la valeur d'appartenance la plus grande
+	    // ici le pas de discretisation va changer...
+	    // Deuxième algo possible
+	    // On pose discrètise l'ensemble des valeurs des f(x)
+	    // On calcule les f-1(x), on prend la plus grande valeur...
+	//}
+    }
+
+    public static class data{
+	public ensemble[] ef;
+	public String[] list(){};
+	public void add(){};
+	public void remove(){};
+    }
+
+    public static class point{
+	double x;
+	double y;
+	public String toString(){
+	    return "x:" + x + "y:" + y+"\n";
+	}
+    }
+    */
+
+/** Use case
+data = new data
+ensemble A;
+ensemble B = A.complementaire()
+*/
+
+
+}
