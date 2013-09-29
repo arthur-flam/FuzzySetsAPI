8d27e8689ef4c9781675e895e72cb88ecf72ed53
Arthur Flam <arthur.flam@gmail.com>
2013-09-29 14:59:00 +0200

    it compiles

--- fuzzySetsAPI.java
+++ fuzzySetsAPI.java
@@ -2,39 +2,46 @@ class fuzzySetsAPI{
 
     public static void main(String[] args){
 	System.out.println("Hello world.");
-	//data data = new data();
+	data d = new data();
     }
     
-    /**
-    public static class ensemble{
+    
+    public static class set{
 	public String name;
-	public point[] points;
+	public element[] elements;
 	
 	//Constructeurs
 	// TODO 
 
 	//Informations
 	public int length(){
-	    return points.length();
+	    return elements.length;
 	};
 	public String toString(){
 		String out = "";
-		//for(int i = 0; i < points.length(); i++){
-		//	out = out + point[i].toString;
-		//}
+		for(int i = 0; i < elements.length; i++){
+		    out = out + elements[i].toString();
+		}
 		return out;
 	}
 	public double valueAt(double x){
-		//Trouver x1 < x < x2
-		// par dichotomie ? A voir en cas d'appels multiples
-		//return (y2-y1)/(x2-x1) * (x1-x);
+	    //Trouver x1 < x < x2
+	    // par dichotomie ? A voir en cas d'appels multiples
+	    //return (y2-y1)/(x2-x1) * (x1-x);
+	    return 0;
 	}
 
 	//Opérations ensemblistes
-	public static ensemble union(ensemble A, ensemble B, String method){}
-	public static ensemble intersection(ensemble A, ensemble B, String method){}
-	public static ensemble complementaire(){}
-	//public static ensemble apply(ensemble A, math.function f){}{
+	public static set union(set A, set B, String method){
+	    return A;
+	}
+	public static set intersection(set A, set B, String method){
+	    return A;
+	}
+	public static set complementaire(set A){
+	    return A;
+	}
+	//public static set apply(ensemble A, math.function f){}{
 	    // Premier algo possible
 	    // calcul des valeurs des points (x,y) de A -> double[][] (f(x), y)
 	    // Si on calcul un f(x) déjà obtenu, on prend la valeur d'appartenance la plus grande
@@ -46,20 +53,27 @@ class fuzzySetsAPI{
     }
 
     public static class data{
-	public ensemble[] ef;
-	public String[] list(){};
+	public set[] sets;
+	public String[] list(){
+	    String[] names;
+	    names = new String[sets.length];
+	    for(int i = 0; i>sets.length; i++){
+		names[i] = sets[i].name;
+	    }
+	    return names;
+	}
 	public void add(){};
 	public void remove(){};
     }
 
-    public static class point{
+    public static class element{
 	double x;
 	double y;
 	public String toString(){
 	    return "x:" + x + "y:" + y+"\n";
 	}
     }
-    */
+    
 
 /** Use case
 data = new data
